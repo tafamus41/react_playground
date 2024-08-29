5fc14a17181a735b29781fc2c7c6660f27b9fa60
asimov <seyranmustafa4@gmail.com>
2024-08-30 00:44:08 +0300

    football

--- rc07-football-legends/starter/src/components/legends/LegendContainer.jsx
+++ rc07-football-legends/starter/src/components/legends/LegendContainer.jsx
@@ -1,15 +1,10 @@
 import Container from "react-bootstrap/Container";
 import { data } from "../../helpers/data";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 
 import LegendCard from "./LegendCard";
 
 const LegendContainer = () => {
-  
-  // console.log(data);
   return (
     <Container>
       <Row>
