5e58052fdc92b4912a84d4742f1a91a3ea85de2c
asimov <seyranmustafa4@gmail.com>
2024-09-04 20:27:15 +0300

    p

--- rc09-lifeCycle/src/App.js
+++ rc09-lifeCycle/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {useState} from "react";
+import LifeCycleMethods from "./LifeCycleMethods";
 
 const App = () => {
+  const[goster,setGoster]=useState(true)
   return (
-    <div>App</div>
-  )
+    <div className="text-center mt-4 container">
+      <button onClick={()=>setGoster(!goster)} className="btn btn-danger">SHOW</button>
+      {goster && <LifeCycleMethods/>}
+    </div>
+   
+  );
 }
 
 export default App
