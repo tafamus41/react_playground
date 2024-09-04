3b77f40f261bce8cdd16e34856411779dc8af252
asimov <seyranmustafa4@gmail.com>
2024-09-04 20:59:22 +0300

    p

--- rc09-lifeCycle/src/Clock.jsx
+++ rc09-lifeCycle/src/Clock.jsx
@@ -1,4 +1,33 @@
-import React from 'react';
+// //?==================================================================
+// //?                       USEEFFECT HOOK
+// //?==================================================================
+// //! The Effect Hook,function componentlerde yan etkiler gerçekleştirmenizi sağlar.
+// //! useEffect Hook'u componentDidMount, componentDidUpdate ve componentWillUnmount olarak düşünebiliriz..setInterval, fetch axios ile veri çekme ve mesela dizi her değiştiğinde set et (yani istediğim bişeyi yap) demek için kullanılır
+
+import { useEffect, useState } from "react";
+
+// //! useEffect(() => {
+// //*   /* ComponentDidMount code */
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   */ componentDidUpdate code */
+// //! }, [var1, var2]);
+
+// //! useEffect(() => {
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   //* componentDidMount code + componentDidUpdate code */
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, [var1, var2]);
 import moment from "moment"
 
 const Clock = () => {
