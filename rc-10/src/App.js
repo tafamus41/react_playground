f268d856a760269b0b0194cccf269790e5a18748
asimov <seyranmustafa4@gmail.com>
2024-09-05 20:31:38 +0300

    p

--- rc-10/src/App.js
+++ rc-10/src/App.js
@@ -1,14 +1,11 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
 // index.html e de bootstrap in js teşekkürünü script olarak ekledik
-
-
-
 
 function App() {
   return (
-    <div >
-   App
+    <div>
+      <Home />
     </div>
   );
 }
