43ef5b65516dbbca12a6489384c846cda0c0cb5a
asimov <seyranmustafa4@gmail.com>
2024-09-05 19:57:02 +0300

    p

---
+++ rc-10/src/App.js
@@ -0,0 +1,16 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+// index.html e de bootstrap in js teşekkürünü script olarak ekledik
+
+
+
+
+function App() {
+  return (
+    <div >
+   App
+    </div>
+  );
+}
+
+export default App;
