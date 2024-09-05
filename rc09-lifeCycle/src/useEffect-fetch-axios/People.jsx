b7ad2a0bb029cffd78de4bd723a4c00618a880e1
asimov <seyranmustafa4@gmail.com>
2024-09-06 01:08:09 +0300

    fetch

--- rc09-lifeCycle/src/useEffect-fetch-axios/People.jsx
+++ rc09-lifeCycle/src/useEffect-fetch-axios/People.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const People = () => {
   const[people,setPeople]=useState([])
 
   //! 1.yol fetch then yapısı
-  
+
+useEffect(()=>{
+  fetch("https://reqres.in/api/users").then((res)=>res.json()).then((veri)=>setPeople(veri.data))
+},[])
+
   return (
     <div>
       People
