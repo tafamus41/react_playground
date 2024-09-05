b5114f66900038698a677a468f6455a11ef08d56
asimov <seyranmustafa4@gmail.com>
2024-09-06 01:23:12 +0300

    p

--- rc09-lifeCycle/src/useEffect-fetch-axios/People.jsx
+++ rc09-lifeCycle/src/useEffect-fetch-axios/People.jsx
@@ -15,7 +15,11 @@ const People = () => {
     <div className="container text-center mt-4">
       <div className="row">
         {people.map((a) => (
-          <div className="col"></div>
+          <div className="col-12 col-sm-6 col-md-4">
+            <img className="rounded" src={a.avatar} alt="" />
+            <h5>{a.first_name}</h5>
+            <h6>{a.last_name}</h6>
+          </div>
         ))}
       </div>
     </div>
