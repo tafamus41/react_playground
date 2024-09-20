553efd60cbe8137ab7088e8998de50bd777a74bf
asimov <seyranmustafa4@gmail.com>
2024-09-20 22:50:15 +0300

    p

--- rc13-usecontext/src/App.js
+++ rc13-usecontext/src/App.js
@@ -1,12 +1,13 @@
-import { useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import Home from "./components/Home"
 import data from "./data"
-
+export const StudentContext=createContext()
 function App() {
-  export const StudentContext=useContext()
+  
   const[students,setStudents]=useState(data)
   return (
     <div className="App">
+      
      <Home/>
     </div>
   );
