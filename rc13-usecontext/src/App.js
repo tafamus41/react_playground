0c62f7498c8e02dce28d30074ee9add272beff05
asimov <seyranmustafa4@gmail.com>
2024-09-20 23:09:11 +0300

    p

--- rc13-usecontext/src/App.js
+++ rc13-usecontext/src/App.js
@@ -4,9 +4,13 @@ import data from "./data";
 export const StudentContext = createContext();
 function App() {
   const [students, setStudents] = useState(data);
+const changeColor=()=>{
+
+}
+
   return (
     <div className="App">
-      <StudentContext.Provider value={{students}}>
+      <StudentContext.Provider value={{students,changeColor}}>
         <Home />
       </StudentContext.Provider>
     </div>
