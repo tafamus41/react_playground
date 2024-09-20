22a5367652ade5c3934170935be7ec7b03ab3532
asimov <seyranmustafa4@gmail.com>
2024-09-20 22:40:56 +0300

    p

--- rc13-usecontext/src/components/OgrenciItem.jsx
+++ rc13-usecontext/src/components/OgrenciItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OgrenciItem = () => {
   return (
     <div>
-        <OgrenciList/>
+        
     </div>
   )
 }
--- rc13-usecontext/src/components/ogrenciList.jsx
+++ rc13-usecontext/src/components/ogrenciList.jsx
@@ -4,7 +4,8 @@ import OgrenciItem from './OgrenciItem'
 const ogrenciList = () => {
   return (
     <div>
-
+<img src="https://gelecekegitimde.com/wp-content/uploads/2015/01/fft99_mf3019637.jpeg" alt="" />
+<h2 style={{textAlign:"center", background:"pink"}}>ÖĞRENCİ LİSTESİ</h2>
        <OgrenciItem/>
     </div>
   )
