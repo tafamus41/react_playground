0763bf6f7c00bdc4e470ba2b8460228ee20479c7
asimov <seyranmustafa4@gmail.com>
2024-09-20 23:03:43 +0300

    p

--- rc13-usecontext/src/components/OgrenciItem.jsx
+++ rc13-usecontext/src/components/OgrenciItem.jsx
@@ -5,7 +5,18 @@ const OgrenciItem = () => {
     const {students}= useContext(StudentContext)
   return (
     <div>
-        
+        {
+            students.map((student)=>(
+           <div key={student.id} style={{background:student.color}}>
+            <h3>
+                NAME:{student.name}
+            </h3>
+            <h4>EMAİL:{student.email}</h4>
+            <h4>AGE:{student.age}</h4>
+            COLOR: <input type="text" value={student.color}/>
+           </div>     
+            ))
+        }
     </div>
   )
 }
