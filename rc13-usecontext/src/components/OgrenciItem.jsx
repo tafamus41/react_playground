e983b6bc777ce70e2a2931f621ecea9d4ecbcea3
asimov <seyranmustafa4@gmail.com>
2024-10-01 09:47:59 +0300

    p

--- rc13-usecontext/src/components/OgrenciItem.jsx
+++ rc13-usecontext/src/components/OgrenciItem.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { StudentContext } from '../App'
 
 const OgrenciItem = () => {
-    const {students}= useContext(StudentContext)
+    const {students,changeColor}= useContext(StudentContext)
   return (
     <div>
         {
--- rc13-usecontext/src/components/ogrenciList.jsx
+++ rc13-usecontext/src/components/ogrenciList.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import OgrenciItem from './OgrenciItem'
+import React from "react";
+import OgrenciItem from "./OgrenciItem";
 
 const ogrenciList = () => {
   return (
     <div>
-<img src="https://gelecekegitimde.com/wp-content/uploads/2015/01/fft99_mf3019637.jpeg" alt="" />
-<h2 style={{textAlign:"center", background:"pink"}}>ÖĞRENCİ LİSTESİ</h2>
-       <OgrenciItem/>
+      <img
+        src="https://gelecekegitimde.com/wp-content/uploads/2015/01/fft99_mf3019637.jpeg"
+        alt=""
+      />
+      <h2 style={{ textAlign: "center", background: "pink" }}>
+        ÖĞRENCİ LİSTESİ
+      </h2>
+      <OgrenciItem />
     </div>
-  )
-}
+  );
+};
 
-export default ogrenciList
+export default ogrenciList;
