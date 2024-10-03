1fea489ced6d0dda07c216319e1f1cfb873515b7
asimov <seyranmustafa4@gmail.com>
2024-10-03 16:56:09 +0300

    p

---
+++ rc19-material-ui/src/App.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+import Home from "./components/Home";
+import data from "./data";
+export const StudentContext = createContext();
+function App() {
+  const [students, setStudents] = useState(data);
+const changeColor=(id,newRenk)=>{
+  setStudents(students.map((a)=>a.id===id ? {...a,color:newRenk}:a))
+
+}
+
+  return (
+    <div className="App">
+      <StudentContext.Provider value={{students,changeColor}}>
+        <Home />
+      </StudentContext.Provider>
+    </div>
+  );
+}
+
+export default App;
