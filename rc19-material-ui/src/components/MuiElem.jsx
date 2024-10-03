5b360bdcc5be69e9cc67f9f81fdee531bf85bce7
asimov <seyranmustafa4@gmail.com>
2024-10-03 17:39:05 +0300

    p

--- rc19-material-ui/src/components/MuiElem.jsx
+++ rc19-material-ui/src/components/MuiElem.jsx
@@ -5,7 +5,8 @@ import React from "react";
 const MuiElem = () => {
   return (
     <Container maxWidth="xl">
-      <Typography>Mui Elements</Typography>
+      <Typography align="center" color="success" variant="h4" variantMapping={{"h4":"h2"}} >Mui Elements</Typography>
+      <Typography align="center" color="success" variant="h5" component="h3" mt={5} >Mui Elements</Typography>
     </Container>
   );
 };
