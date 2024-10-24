f699d9c1dc7245b388f462d378a374dd711304dd
asimov <seyranmustafa4@gmail.com>
2024-10-24 17:28:12 +0300

    p

--- rc07-football-legends/starter/src/components/legends/LegendCard.jsx
+++ rc07-football-legends/starter/src/components/legends/LegendCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
--- rc07-football-legends/starter/src/components/legends/LegendContainer.jsx
+++ rc07-football-legends/starter/src/components/legends/LegendContainer.jsx
@@ -1,8 +1,8 @@
 import { data } from "../../helpers/data";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+// import Col from "react-bootstrap/Col";
+// import Card from "react-bootstrap/Card";
 import { useState } from "react";
 import LegendCard from "./LegendCard";
 import Form from "react-bootstrap/Form";
