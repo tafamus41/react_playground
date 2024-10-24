bcc1ab4a9b1b23a24fd934f0186480c2f12d6b3a
asimov <seyranmustafa4@gmail.com>
2024-10-25 01:20:11 +0300

    p

--- rc08-appointment app/src/components/Doctors.jsx
+++ rc08-appointment app/src/components/Doctors.jsx
@@ -1,5 +1,7 @@
 import Container from "react-bootstrap/Container";
-import {doctorData} from "../helper/data";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { doctorData } from "../helper/data";
 
 const Doctors = () => {
   return (
@@ -7,8 +9,17 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {doctorData.map(({ id, img, dep, name }) => (
+          <Col key={id}>
+            <img src={img} alt={name} />
+            <h5>{name}</h5>
+            <h6>{dep}</h6>
+          </Col>
+        ))}
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
