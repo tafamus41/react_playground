da37781642f77d08d0872d8641e3823b84b3382f
asimov <seyranmustafa4@gmail.com>
2024-10-25 02:44:02 +0300

    p

--- rc08-appointment app/src/components/AppointmentList.jsx
+++ rc08-appointment app/src/components/AppointmentList.jsx
@@ -1,6 +1,6 @@
 import Container from "react-bootstrap/Container"
 
-const AppointmentList = () => {
+const AppointmentList = ({appointments}) => {
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
--- rc08-appointment app/src/pages/Home.jsx
+++ rc08-appointment app/src/pages/Home.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import AppointmentList from "../components/AppointmentList"
 import Doctors from "../components/Doctors"
+import {appointmentData} from "../helper/data"
 
 const Home = () => {
-  return (
+  const [appointments, setAppointments] = useState(appointmentData)
+return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
       <Doctors />
-      <AppointmentList />
+      <AppointmentList appointments={appointments}/>
     </main>
   )
 }
