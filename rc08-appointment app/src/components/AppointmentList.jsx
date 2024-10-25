95a4c5938d94d5d756419a0ae06cd3dd56f40098
asimov <seyranmustafa4@gmail.com>
2024-10-25 04:06:41 +0300

    p

--- rc08-appointment app/src/components/AppointmentList.jsx
+++ rc08-appointment app/src/components/AppointmentList.jsx
@@ -3,12 +3,13 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { TiDelete } from "react-icons/ti";
 
-const AppointmentList = ({ appointments }) => {
+const AppointmentList = ({ appointments,handleDelete }) => {
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      {appointments.length===0 && <img src="./img/appointment.jpg" width="70%" alt=""/>}
       {appointments.map(({ id, patient, consulted, doctor, day }) => (
         <div className={consulted?"appointments consulted":"appointments"} >
           <Row className="justify-content-between align-items-center" >
@@ -23,7 +24,7 @@ const AppointmentList = ({ appointments }) => {
               <h4>Time:{new Date(day).toLocaleTimeString("tr")}</h4>
             </Col>
             <Col className="text-end" >
-              <TiDelete className="text-danger display-3" type="button"/>
+              <TiDelete className="text-danger display-3" type="button" onClick={()=>handleDelete(id)} />
             </Col>
           </Row>
         </div>
--- rc08-appointment app/src/pages/Home.jsx
+++ rc08-appointment app/src/pages/Home.jsx
@@ -8,12 +8,15 @@ const Home = () => {
 const handleAdd=(newAppointment)=>{
 setAppointments([...appointments,newAppointment])
 }
-
+const handleDelete=(id)=>{
+const filteredList=appointments.filter(item=>item.id !== id)
+setAppointments(filteredList)
+}
 return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
       <Doctors handleAdd={handleAdd} />
-      <AppointmentList appointments={appointments}/>
+      <AppointmentList appointments={appointments} handleDelete={handleDelete} />
     </main>
   )
 }
