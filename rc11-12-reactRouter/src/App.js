71b9bee86e4901cbf064732ebb4d4f10ef278ded
asimov <seyranmustafa4@gmail.com>
2024-10-27 02:47:35 +0300

    p

--- rc11-12-reactRouter/src/App.js
+++ rc11-12-reactRouter/src/App.js
@@ -1,27 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from "./pages/Home";
-import MyNavbar from "./components/MyNavbar";
-import Teacher from "./pages/Teacher";
-import CourseCard from "./pages/CourseCard";
-import ContactForm from "./pages/ContactForm";
-import Paths from "./pages/Paths";
+import AppRouter from "./router/AppRouter";
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <MyNavbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/teacher" element={<Teacher/>}/>
-        <Route path="/courses" element={<CourseCard/>}/>
-        <Route path="/contact" element={<ContactForm/>}/>
-        <Route path="/paths" element={<Paths/>}/>
-        
-      </Routes>
-      </BrowserRouter>
-
+      <AppRouter/>
     </div>
   );
 }
---
+++ rc11-12-reactRouter/src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import MyNavbar from "./components/MyNavbar";
+import Teacher from "./pages/Teacher";
+import CourseCard from "./pages/CourseCard";
+import ContactForm from "./pages/ContactForm";
+import Paths from "./pages/Paths";
+const AppRouter = () => {
+  return (
+    <div>
+        <BrowserRouter>
+      <MyNavbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/teacher" element={<Teacher/>}/>
+        <Route path="/courses" element={<CourseCard/>}/>
+        <Route path="/contact" element={<ContactForm/>}/>
+        <Route path="/paths" element={<Paths/>}/>
+        
+      </Routes>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default AppRouter
