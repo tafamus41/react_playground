65ac7d24e9a81af2e046db2ab50cc1219ae0b2e4
asimov <seyranmustafa4@gmail.com>
2024-10-27 02:49:23 +0300

    p

--- rc11-12-reactRouter/src/router/AppRouter.jsx
+++ rc11-12-reactRouter/src/router/AppRouter.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from "./pages/Home";
-import MyNavbar from "./components/MyNavbar";
-import Teacher from "./pages/Teacher";
-import CourseCard from "./pages/CourseCard";
-import ContactForm from "./pages/ContactForm";
-import Paths from "./pages/Paths";
+import Home from "../pages/Home";
+import MyNavbar from "../components/MyNavbar";
+import Teacher from "../pages/Teacher";
+import CourseCard from "../pages/CourseCard";
+import ContactForm from "../pages/ContactForm";
+import Paths from "../pages/Paths";
 const AppRouter = () => {
   return (
     <div>
