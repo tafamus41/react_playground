292e5b2a5bce10265d84b060534cf277fd5242d6
asimov <seyranmustafa4@gmail.com>
2024-10-27 01:58:29 +0300

    p

--- rc11-12-reactRouter/src/App.js
+++ rc11-12-reactRouter/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./pages/Home";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <MyNavbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route/>
