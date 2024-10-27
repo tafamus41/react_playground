bca3daf168ea8c8261c4a41e6e74e6d41e950b91
asimov <seyranmustafa4@gmail.com>
2024-10-27 03:20:43 +0300

    p

--- rc11-12-reactRouter/src/router/AppRouter.jsx
+++ rc11-12-reactRouter/src/router/AppRouter.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import MyNavbar from "../components/MyNavbar";
 import Teacher from "../pages/Teacher";
 import CourseCard from "../pages/CourseCard";
 import ContactForm from "../pages/ContactForm";
 import Paths from "../pages/Paths";
-import NotFound from '../pages/NotFound';
+import NotFound from "../pages/NotFound";
 const AppRouter = () => {
   return (
     <div>
-        <BrowserRouter>
-      <MyNavbar/>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/teacher" element={<Teacher/>}/>
-        <Route path="/courses" element={<CourseCard/>}/>
-        <Route path="/contact" element={<ContactForm/>}/>
-        <Route path="/paths" element={<Paths/>}/>
-        <Route path='*' element={<NotFound/>} />
-      </Routes>
+      <BrowserRouter>
+        <MyNavbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/teacher" element={<Teacher />} />
+          
+          <Route>
+            <Route path="/courses" element={<CourseCard />} />
+          </Route>
+
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/paths" element={<Paths />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
-
     </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
