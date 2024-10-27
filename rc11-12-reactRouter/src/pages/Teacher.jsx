4216a81a47a5455891fd3245f3dc6f248183694a
asimov <seyranmustafa4@gmail.com>
2024-10-27 16:19:37 +0300

    p

--- rc11-12-reactRouter/src/pages/Teacher.jsx
+++ rc11-12-reactRouter/src/pages/Teacher.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Teacher = () => {
-  return (
-    <div>Teacher</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Teacher
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setPeople(res.data));
+  }, []);
+  return (
+    <div className="container text-center mt-4">
+      <div className="row">
+        {people.map((person) => (
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={person.id}>
+            {/* https://www.dicebear.com/styles/avataaars/ */}
+
+            <img
+              src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${person.name}`}
+              alt=""
+              onClick={() =>
+                navigate(`/teacher/${person.id}`, { state: { person } })
+              }
+            />
+
+            <h5>{person.name}</h5>
+            <h6>{person.username} </h6>
+            <h4>{person.phone} </h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Teacher;
