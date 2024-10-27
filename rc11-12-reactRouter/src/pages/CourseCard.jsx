195ffaea8c460b285784c07d8d33e6bfcda12782
asimov <seyranmustafa4@gmail.com>
2024-10-27 03:48:13 +0300

    p

--- rc11-12-reactRouter/src/pages/CourseCard.jsx
+++ rc11-12-reactRouter/src/pages/CourseCard.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-
+import data from "../data"
+import { useNavigate } from 'react-router-dom';
 const CourseCard = () => {
+    const navigate=useNavigate()
   return (
     <Container>
       <Row className="g-3 text-center">
-        {/* {data.map((a) => {
-          const{id,name,img,text}=a */}
+        {data.map((a) => {
+          const{id,name,img,text}=a
        
 
           return (
             <Col
               className="d-flex justify-content-center col-sm-12 col-md-6 col-lg-4"
-            //   key={id}
+              key={id}
             >
               <Card style={{ width: "18rem" }}>
                 <Card.Img variant="top" 
-                // src={img} 
+                src={img} 
                 />
                 <Card.Body>
                   <Card.Title className="text-danger">
-                    {/* {name}  */}
+                    {name} 
                     </Card.Title>
                   <Card.Text>
-                    {/* {text} */}
+                    {text}
                     </Card.Text>
                   <Button
-// onClick={()=>navigate(`/courses/${name}`)}
+onClick={()=>navigate(`/courses/${name}`)}
 variant="primary">DETAILS</Button>
                 </Card.Body>
               </Card>
             </Col>
           );
-        {/* })} */}
+         })} 
       </Row>
     </Container>
   )
