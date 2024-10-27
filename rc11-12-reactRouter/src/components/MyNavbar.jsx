3899fa4a32bae2e974445f38ee20f45321e7b707
asimov <seyranmustafa4@gmail.com>
2024-10-27 03:18:40 +0300

    p

--- rc11-12-reactRouter/src/components/MyNavbar.jsx
+++ rc11-12-reactRouter/src/components/MyNavbar.jsx
@@ -13,14 +13,44 @@ const MyNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink 
-            style={({isActive})=> ({color:isActive&&"red"})}
-            className="nav-link" to="/">Home</NavLink>
-            <NavLink style={({isActive})=> ({fontSize:isActive&&"50px"})} to="/teacher" className="nav-link" >Teacher</NavLink>
-            <NavLink style={({isActive})=> ({border:isActive&&"1px solid blue"})} to="/courses" className="nav-link" >Courses</NavLink>
-            <NavLink style={({isActive})=> ({backgroundColor:isActive&&"salmon"})} to="/contact" className="nav-link" >Contact</NavLink>
-            <NavLink to="/paths" className="nav-link" >Paths</NavLink>
-            <Nav.Link href="https://github.com/tafamus41">GitHub</Nav.Link>
+            <NavLink
+              style={({ isActive }) => ({ color: isActive && "red" })}
+              className="nav-link"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              style={({ isActive }) => ({ fontSize: isActive && "50px" })}
+              to="/teacher"
+              className="nav-link"
+            >
+              Teacher
+            </NavLink>
+            <NavLink
+              style={({ isActive }) => ({
+                border: isActive && "1px solid blue",
+              })}
+              to="/courses"
+              className="nav-link"
+            >
+              Courses
+            </NavLink>
+            <NavLink
+              style={({ isActive }) => ({
+                backgroundColor: isActive && "salmon",
+              })}
+              to="/contact"
+              className="nav-link"
+            >
+              Contact
+            </NavLink>
+            <NavLink to="/paths" className="nav-link">
+              Paths
+            </NavLink>
+            <Nav.Link target="blank" href="https://github.com/tafamus41">
+              GitHub
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
