e39310f63198ff4545e54c02f33cd34b26bd03b3
asimov <seyranmustafa4@gmail.com>
2024-10-28 02:20:43 +0300

    p

--- rc13-usecontext/context-1/src/App.js
+++ rc13-usecontext/context-1/src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import Home from "./components/Home";
 import data from "./data";
 export const StudentContext = createContext();
