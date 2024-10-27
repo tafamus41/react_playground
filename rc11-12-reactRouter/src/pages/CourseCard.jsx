4731383a29aa8ea74c49ba477875df71b548dfb8
asimov <seyranmustafa4@gmail.com>
2024-10-27 03:39:58 +0300

    p

--- rc11-12-reactRouter/src/pages/CourseCard.jsx
+++ rc11-12-reactRouter/src/pages/CourseCard.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const CourseCard = () => {
   return (
-    <div>CourseCard</div>
+    <Container>
+      <Row className="g-3 text-center">
+        {/* {data.map((a) => {
+          const{id,name,img,text}=a */}
+       
+
+          return (
+            <Col
+              className="d-flex justify-content-center col-sm-12 col-md-6 col-lg-4"
+            //   key={id}
+            >
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" 
+                // src={img} 
+                />
+                <Card.Body>
+                  <Card.Title className="text-danger">
+                    {/* {name}  */}
+                    </Card.Title>
+                  <Card.Text>
+                    {/* {text} */}
+                    </Card.Text>
+                  <Button
+// onClick={()=>navigate(`/courses/${name}`)}
+variant="primary">DETAILS</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        {/* })} */}
+      </Row>
+    </Container>
   )
 }
 
