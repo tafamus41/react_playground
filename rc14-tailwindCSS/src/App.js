a7c0ca0894d3e1bb6366acab2147dd328f94f06a
asimov <seyranmustafa4@gmail.com>
2024-10-28 19:10:21 +0300

    p

--- rc14-tailwindCSS/src/App.js
+++ rc14-tailwindCSS/src/App.js
@@ -1,7 +1,20 @@
+import FlexAndResponsive from "./components/FlexAndResponsive";
+import HoverAndFocus from "./components/HoverAndFocus";
+import Navbar from "./components/Navbar";
+import ProfileCard from "./components/ProfileCard";
+import SpacingAndSizing from "./components/SpacingAndSizing";
+import Typography from "./components/Typography";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Cohort 17</h1>
+    <div className="dark:bg-gray-600 min-h-screen">
+      <Navbar />
+      <h1>Merhaba Tailwind</h1>
+      <ProfileCard />
+      {/* <Typography /> */}
+      {/* <SpacingAndSizing /> */}
+      {/* <HoverAndFocus /> */}
+      {/* <FlexAndResponsive /> */}
     </div>
   );
 }
