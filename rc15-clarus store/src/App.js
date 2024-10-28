aae3533710c42a9bd975f129e77a0cda66916210
asimov <seyranmustafa4@gmail.com>
2024-10-29 02:45:15 +0300

    p

---
+++ rc15-clarus store/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+      <span className="animate-bounce">🏀</span>
+      <h1 className="animate-pulse">
+        Welcome to Session 1 of the Clarus Store Project FS17!
+      </h1>
+      <span className="animate-bounce">🏀</span>
+    </div>
+  );
+}
+
+export default App;
