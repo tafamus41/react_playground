ee7428e9f76140e7f733e4b637530744f0d64fc8
asimov <seyranmustafa4@gmail.com>
2024-10-28 05:16:47 +0300

    p

--- rc13-usecontext/context-2/src/context/KullaniciProvider.jsx
+++ rc13-usecontext/context-2/src/context/KullaniciProvider.jsx
@@ -16,23 +16,13 @@ useEffect(()=>{
 fetch("https://api.github.com/users").then((res)=>res.json()).then((veri)=>setUsers(veri)  )
 },[])
 
-
 const changeWidth=(id,widtH)=>{
-
-
 setUsers(users.map((a)=>a.id===id  ? {...a, width: widtH} : a))
-
-
 }
-
-
-
-
   return (
     <KullaniciContext.Provider value={{users,changeWidth}}>
 {children}
     </KullaniciContext.Provider>
   )
 }
-
 export default KullaniciProvider
