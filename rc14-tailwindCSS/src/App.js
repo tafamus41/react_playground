986382375725c7a16cbead8acae555e5cf46968f
asimov <seyranmustafa4@gmail.com>
2024-10-29 00:19:46 +0300

    p

--- rc14-tailwindCSS/src/App.js
+++ rc14-tailwindCSS/src/App.js
@@ -8,13 +8,13 @@ import Typography from "./components/Typography";
 function App() {
   return (
     <div className="dark:bg-gray-600 min-h-screen">
-      {/* <Navbar /> */}
-      <h1 className="text-2xl text-center text-purple-700 font-bold">Merhaba Tailwind</h1>
+      <Navbar />
+      <h1 className="text-2xl text-center text-purple-700 font-bold dark:text-white font-bold">Merhaba Tailwind</h1>
       <ProfileCard />
       {/* <Typography /> */}
       {/* <SpacingAndSizing /> */}
       {/* <HoverAndFocus /> */}
-      <FlexAndResponsive />
+      {/* <FlexAndResponsive /> */}
     </div>
   );
 }
--- rc14-tailwindCSS/src/components/FlexAndResponsive.jsx
+++ rc14-tailwindCSS/src/components/FlexAndResponsive.jsx
@@ -1,30 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const FlexAndResponsive = () => {
+  
   return (
     <div>
-      <div className='flex flex-row flex-wrap' >
-        <div className=' basis-1/4 bg-fuchsia-200'>
-          01
-        </div>
-        <div className='basis-1/4 bg-fuchsia-300'>
-          02
-        </div>
-        <div className='basis-1/4 bg-fuchsia-400'>
-          03
-        </div>
-        <div className='basis-1/4 bg-fuchsia-500'>
-          04
-        </div>
-        <div className='basis-2/4 bg-fuchsia-600'>
-          03
-        </div>
-        <div className='basis-2/4 bg-fuchsia-700'>
-          04
-        </div>
+      <div className="flex flex-row flex-wrap">
+        <div className=" basis-1/4 bg-fuchsia-200">01</div>
+        <div className="basis-1/4 bg-fuchsia-300">02</div>
+        <div className="basis-1/4 bg-fuchsia-400">03</div>
+        <div className="basis-1/4 bg-fuchsia-500">04</div>
+        <div className="basis-2/4 bg-fuchsia-600">03</div>
+        <div className="basis-2/4 bg-fuchsia-700">04</div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+{Array.from({length:8},(_,item)=>(
+  <div className="bg-teal-500">{item}</div>
+))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FlexAndResponsive
+export default FlexAndResponsive;
