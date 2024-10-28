1d6a5135654940e95e35cd0725d5e1330d0e207c
asimov <seyranmustafa4@gmail.com>
2024-10-28 04:58:58 +0300

    p

--- rc13-usecontext/context-2/src/component/AnaUser.jsx
+++ rc13-usecontext/context-2/src/component/AnaUser.jsx
@@ -1,37 +1,25 @@
-import React from 'react'
-import { useContext } from 'react'
-import { KullaniciContext } from '../context/KullaniciProvider'
-
+import React from "react";
+import { useContext } from "react";
+import { KullaniciContext } from "../context/KullaniciProvider";
 const AnaUser = () => {
-const {users, changeWidth}= useContext(KullaniciContext)
-
+  const { users, changeWidth } = useContext(KullaniciContext);
   return (
     <div>
-{users.map((a)=>(
-  <div>
-    <h3>
-      {a.login}
-    </h3>
-
-    <img src={a.avatar_url} alt="" width={a.width ? a.width : "300px"}/>
-
-<div>
-  <label htmlFor="">Image width (px)</label>
-  <input type="number" 
-
-onChange={(e)=>changeWidth(a.id, e.target.value)}
-
-  />
-</div>
-
-
-
-
-  </div>
-))}
-
+      {users.map((a) => (
+        <div>
+          <h3>{a.login}</h3>
+          <img src={a.avatar_url} alt="" width={a.width ? a.width : "300px"} />
+          <div>
+            <label htmlFor="">Image width (px)</label>
+            <input
+              type="number"
+              onChange={(e) => changeWidth(a.id, e.target.value)}
+            />
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default AnaUser
+export default AnaUser;
