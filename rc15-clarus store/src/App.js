923053bf37b12721587d7ce8436bb7c96a690246
asimov <seyranmustafa4@gmail.com>
2024-10-29 03:08:31 +0300

    p

--- rc15-clarus store/src/App.js
+++ rc15-clarus store/src/App.js
@@ -1,12 +1,16 @@
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
-      <span className="animate-bounce">🏀</span>
-      <h1 className="animate-pulse">
-        Welcome to Session 1 of the Clarus Store Project FS17!
-      </h1>
-      <span className="animate-bounce">🏀</span>
-    </div>
+
+    <Login/>
+    // <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+    //   <span className="animate-bounce">🏀</span>
+    //   <h1 className="animate-pulse">
+    //     Welcome to Session 1 of the Clarus Store Project FS17!
+    //   </h1>
+    //   <span className="animate-bounce">🏀</span>
+    // </div>
   );
 }
 
