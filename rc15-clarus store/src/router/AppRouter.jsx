005075b89f39db60dbc94146e9aab5d0da447017
asimov <seyranmustafa4@gmail.com>
2024-10-30 00:54:05 +0300

    p

--- rc15-clarus store/src/router/AppRouter.jsx
+++ rc15-clarus store/src/router/AppRouter.jsx
@@ -4,20 +4,17 @@ import Home from "../pages/Home";
 import Products from "../pages/Products";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
-import Navbar from "../components/Navbar";
-import PrivateRouter from "./PrivateRouter"
+import PrivateRouter from "./PrivateRouter";
 const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="" element={<PrivateRouter/>} >
+        <Route path="" element={<PrivateRouter />}>
           <Route path="/dashboard" element={<Home />} />
           <Route path="/dashboard/products" element={<Products />} />
           <Route path="/dashboard/about" element={<About />} />
         </Route>
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
