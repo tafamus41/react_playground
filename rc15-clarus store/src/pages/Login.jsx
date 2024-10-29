468b5035119ae9410cc512ed669601c24b2b2ce2
asimov <seyranmustafa4@gmail.com>
2024-10-29 03:36:45 +0300

    p

--- rc15-clarus store/src/pages/Login.jsx
+++ rc15-clarus store/src/pages/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="loginDiv">
-    <div>
-
+  return (
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center">
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <span className="w-[6px] h-[39px] bg-yellow-500"></span>
+          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase">Clarus Store</h1>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
--- rc15-clarus store/tailwind.config.js
+++ rc15-clarus store/tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsx,js,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#FEAF00",
+        navbarColor: "#F2EAE1",
+        labelColor: "#6C6C6C",
+        white: "#fff",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        label: [
+          "14px",
+          {
+            lineHeight: "normal",
+            fontWeight: "500",
+          },
+        ],
+      },
+      boxShadow: {
+        login: "2px 5px 10px 0px rgba(0, 0, 0, 0.1)",
+      },
+    },
   },
   plugins: [],
-}
+};
+
+
+
+
+
