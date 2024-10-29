e9d37b6aeacc8897428f0e630cf5816ae442b203
asimov <seyranmustafa4@gmail.com>
2024-10-29 16:59:44 +0300

    p

--- rc15-clarus store/src/components/Navbar.jsx
+++ rc15-clarus store/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='bg-navbarColor md:text-sm'>
+      <div>
+        <div>
+          <a href="https://clarusway.com" target='true' className='flex items-center' ><img src={logo} alt="clarusway" width={50} height={50} /><span>Clarusway</span></a>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- rc15-clarus store/src/router/AppRouter.jsx
+++ rc15-clarus store/src/router/AppRouter.jsx
@@ -4,20 +4,21 @@ import Home from "../pages/Home";
 import Products from "../pages/Products";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
-
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/products" element={<Products/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
