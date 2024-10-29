18b3a7e2996659bfc35e6d545798e6680afcbace
asimov <seyranmustafa4@gmail.com>
2024-10-29 15:11:23 +0300

    p

--- rc15-clarus store/src/pages/Login.jsx
+++ rc15-clarus store/src/pages/Login.jsx
@@ -8,10 +8,16 @@ const Login = () => {
           <span className="w-[6px] h-[39px] bg-yellow-500"></span>
           <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase">Clarus Store</h1>
         </div>
-        <div>
-          <h3>SIGN IN</h3>
-          <p>Enter your credetentials to access your account</p>
+        <div className="my-2 ">
+          <h3 className="font-montserrat font-[600] text-[22px]">SIGN IN</h3>
+          <p className="font-montserrat text-labelColor text-label mt-1">Enter your credetentials to access your account</p>
         </div>
+        <form className="flex flex-col text-left p-3 gap-5">
+          <div className="flex flex-col gap-2 ">
+            <label className="font-montserrat text-label text-labelColor hover:after:content-['admin@aa.com'] hover:after:text-black hover:after:pl-3 hover:after:underline hover:cursor-pointer" htmlFor="email">Email</label>
+            <input className="login-input" type="email"/>
+          </div>
+        </form>
       </div>
     </div>
   );
