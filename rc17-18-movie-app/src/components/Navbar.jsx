fdfc464a2fffc01157bf939b1229325a8901f7d0
asimov <seyranmustafa4@gmail.com>
2024-10-30 22:33:57 +0300

    p

--- rc17-18-movie-app/src/components/Navbar.jsx
+++ rc17-18-movie-app/src/components/Navbar.jsx
@@ -12,12 +12,14 @@ function classNames(...classes) {
 }
 
 const Navbar = () => {
+  const currentUser={displayName:tafamus41}
   return (
     <Disclosure as="nav" className="bg-neutral-100 dark:bg-gray-900 dark:text-white fixed top-0 w-full z-20">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <Link to="/"> Movie App</Link>
+          <Link className="pr-2 text-2xl font-semibold" to="/"> Movie App</Link>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <h5>Current User</h5>
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
