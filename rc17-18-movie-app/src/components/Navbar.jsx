04f179a5a194de88c09ca5eb035341ee13096c27
asimov <seyranmustafa4@gmail.com>
2024-10-30 22:53:10 +0300

    p

--- rc17-18-movie-app/src/components/Navbar.jsx
+++ rc17-18-movie-app/src/components/Navbar.jsx
@@ -7,10 +7,7 @@ import {
 } from "@headlessui/react";
 import { Link } from "react-router-dom";
 import SwitchButton from "./SwitchButton";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import avatar from "../assets/icons/avatar.png"
 
 const Navbar = () => {
   const currentUser={displayName:"tafamus41"}
@@ -23,14 +20,14 @@ const Navbar = () => {
             {currentUser &&<h5 className="mr-2 capitalize">{currentUser?.displayName}</h5>}
             <SwitchButton/>
             {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
+            <Menu as="div" className="relative">
               <div>
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5" />
+                  
                   <span className="sr-only">Open user menu</span>
                   <img
                     alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={currentUser?.photoURL || avatar}
                     className="h-8 w-8 rounded-full"
                   />
                 </MenuButton>
@@ -40,28 +37,27 @@ const Navbar = () => {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/register"
                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                   >
-                    Your Profile
-                  </a>
+                    Register
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/login"
                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                   >
-                    Settings
-                  </a>
+                    Login
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  <span
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 cursor-pointer"
                   >
-                    Sign out
-                  </a>
+                    Log out
+                  </span>
                 </MenuItem>
               </MenuItems>
             </Menu>
