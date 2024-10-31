1f7b2678cda974e6f8572c2da3fe2da0ab8fddf5
asimov <seyranmustafa4@gmail.com>
2024-10-31 20:30:02 +0300

    p

--- rc17-18-movie-app/src/pages/Main.jsx
+++ rc17-18-movie-app/src/pages/Main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useMovieContext } from "../context/MovieProvider";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 
 const Main = () => {
-  return <div>Main</div>;
+ const {movies,loading}= useMovieContext()
+  return <>
+  <div>
+    {movies.map((movie)=>(
+      <p>{movie.title}</p>
+    ))}
+  </div>
+  </>;
 };
 
 export default Main;
