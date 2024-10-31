49553710fb4039158c01e07d675a93e403d5857a
asimov <seyranmustafa4@gmail.com>
2024-10-31 20:10:17 +0300

    p

--- rc17-18-movie-app/src/pages/Main.jsx
+++ rc17-18-movie-app/src/pages/Main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+
 const Main = () => {
   return <div>Main</div>;
 };
