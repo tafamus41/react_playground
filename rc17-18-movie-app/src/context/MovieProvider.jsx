94f4cd777659df2beac0d927044e7903a64af744
asimov <seyranmustafa4@gmail.com>
2024-10-31 20:24:27 +0300

    p

--- rc17-18-movie-app/src/context/MovieProvider.jsx
+++ rc17-18-movie-app/src/context/MovieProvider.jsx
@@ -1,12 +1,31 @@
-import React, { createContext, useContext } from "react";
-
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios"
 export const MovieContext = createContext();
 
 export const useMovieContext = () => {
   return useContext(MovieContext);
 };
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const MovieProvider = ({ children }) => {
+
+const [movies,setMovies]=useState([])
+const [loading, setLoading] = useState(false);
+useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (URL) => {
+    setLoading(true);
+    axios
+      .get(URL)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
+
+
   return <MovieContext.Provider value={null}>{children}</MovieContext.Provider>;
 };
 
